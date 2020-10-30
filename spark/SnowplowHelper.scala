1a546521d775b831faefd6279eea765284797216
LiveIntent <bingtian.x@gmail.com>
2020-10-30 14:25:14 +0100

    add test jobs

---
+++ spark/SnowplowHelper.scala
@@ -0,0 +1,29 @@
+package com.liveintent.dwh.fragments
+
+import com.liveintent.dwh.logs.snowplow.models.Snowplow
+
+
+object SnowplowHelper {
+
+  private def removeScrapers[P](data: RDD[P], snowplowExtractor: P => Snowplow): RDD[P] = {
+
+    data.map{ p => (snowplowExtractor(p).domain_userid.value,p) }
+      .leftOuterJoin(
+        data
+          .map(snowplowExtractor)
+          .filter { s => s.lidid.nonEmpty && s.domain_userid.nonEmpty }
+          .map( s => (s.domain_userid.value, s.lidid.value) )
+          .groupBy(_._1)
+          .mapValues{ pairs => {
+              val firstElem = pairs.next()
+              pairs.dropWhile( _ == firstElem ).take(1)
+            }
+          }
+          .map{ case (domain_userid, _) => (domain_userid, 1) }
+      )
+      .flatMap{ 
+        case (_, (s, None) ) => Some(s) 
+        case (_, (s, Some(_)) ) => None 
+      }
+
+}
